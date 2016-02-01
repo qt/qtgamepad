81a301805b1157e7e8ef21c3332a6ca7706bad3f
Laszlo Agocs <laszlo.agocs@theqtcompany.com>
2016-02-01 15:26:15 +0100

    We mean it.
    Change-Id: I10339bb43be609bcf8625b740631b591069afbd6
    Reviewed-by: Andy Nichols <andy.nichols@theqtcompany.com>
    Reviewed-by: BogDan Vatra <bogdan@kdab.com>

--- src/gamepad/qgamepadbackend_p.h
+++ src/gamepad/qgamepadbackend_p.h
@@ -37,6 +37,17 @@
 #ifndef QGAMEPADBACKEND_H
 #define QGAMEPADBACKEND_H
 
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #include <QtGamepad/qtgamepadglobal.h>
 
 #include <QtCore/QObject>
